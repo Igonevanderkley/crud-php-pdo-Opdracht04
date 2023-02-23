9d4c3b69e82d1f8aee323d91c29021678616023b
Igone van_der_Kley <igone@vdkley.nl>
2023-02-23 16:25:25 +0100

    added index.php

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+        <h1>Bling Bling Nail Studio Chantal</h1>
+
+        <form action="create.php" method="post">
+
+            <label for="kleur1">Kies 4 basiskleuren voor uw nagels:</label><br>
+            <input type="color" name="kleur1" id="kleur1">
+
+
+            <label for="kleur2"></label><br>
+            <input type="color" name="kleur2" id="kleur2">
+
+
+            <label for="kleur3"></label><br>
+            <input type="color" name="kleur3" id="kleur3">
+
+
+            <label for="kleur4"></label><br>
+            <input type="color" name="kleur4" id="kleur4">
+
+            <br>
+
+            <label for="telnummer">Uw telefoonnummer:</label><br>
+            <input type="tel" name="telnummer" id="telnummer"><br>
+
+            <label for="mail">Uw e-mailadres:</label><br>
+            <input type="email" name="mail" id="mail"><br>
+
+            <label for="date">Afspraak datum:</label><br>
+            <input type="datetime-local" name="date" id="date"><br>
+
+            <input type="checkbox" id="option" name="option" value="Nagelbijt arangement">
+            <label for="option"> Nagelbijt arrangement (termijnbetaling mogelijk) $180</label><br>
+            <input type="checkbox" id="option" name="option" value="Luxe manicure">
+            <label for="option"> Luxe manicure (massage en handpakking) $30,00</label><br>
+            <input type="checkbox" id="option" name="option" value="Nagelreparatie">
+            <label for="option"> Nagelreparatie per nagel (in eerste week gratis) $5,00</label><br><br>
+
+            <input type="hidden" name="send_time" id="send_time">
+
+            <input type="submit" value="Sla op">
+            <input type="reset" value="Reset">
+
+            <script>
+                document.getElementById('send_time').value = new Date().toLocaleString();
+            </script>
+
+
+
+        </form>
+    </body>
+
+</html>
